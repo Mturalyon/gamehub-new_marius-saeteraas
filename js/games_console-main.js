2f6b99dc4af707e234a9c672a9e348b777b0b99c
Marius Sæteraas <71336754+Mturalyon@users.noreply.github.com>
2021-04-17 18:48:41 +0200

    added rest api to console mains

---
+++ js/games_console-main.js
@@ -0,0 +1,27 @@
+const container = document.querySelector(".games-many_container");
+
+const url = "http://saeteraas.one/wp-json/wc/store/products";
+
+async function callApis() {
+
+    try {
+
+        const response = await fetch(url);
+        const json = await response.json();
+
+        console.log(json);
+
+        container.innerHTML = "";
+
+        for (let i = 0; i < json.length; i++) {
+            container.innerHTML += `<a href="games.html?id=${json[i].id}"><img src="${json[i].images[0].src}"
+            alt="picture link to video game site"></a>`;
+        };
+    }
+    catch (error) {
+        container.innerHTML = "an error has occured";
+    }
+
+};
+
+callApis();
