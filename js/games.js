e24dd0bc000bc903db16b45989a9ab145a1b6664
Marius Sæteraas <71336754+Mturalyon@users.noreply.github.com>
2021-04-17 20:29:34 +0200

    added on page navbar function

--- js/games.js
+++ js/games.js
@@ -1,6 +1,11 @@
 const container = document.querySelector(".games-specific-wrapper");                       //targeting container for the games info
 const header = document.querySelector(".game-header_green");                               //targeting category header
 
+const navXbox = document.querySelector(".games-nav ul li:nth-child(1) a");
+const navPlaystation = document.querySelector(".games-nav ul li:nth-child(2) a");
+const navNintendo = document.querySelector(".games-nav ul li:nth-child(3) a");
+const navPc = document.querySelector(".games-nav ul li:nth-child(4) a");
+
 const queryString = document.location.search;                                              //targeting querystring
 const params = new URLSearchParams(queryString);                                           //searching parameters of querystring
 const id = params.get("id");                                                               //catching the value of the "id" parameter
@@ -10,12 +15,12 @@ const url = "https://saeteraas.one/wp-json/wc/store/products/" + id;            
 async function callApi() {                                                                 //calling API function
 
     try {
-        header.innerHTML = "";
         const response = await fetch(url);                                                 //fetching URL
         const json = await response.json();                                                //fetching JSON from URL
-
         console.log(json);
+
         createHTML(json);                                                                  //calling a separate function that creates the HTML
+        navOrganize(json);
     }
     catch (error) {                                                                        //catching and displaying errors
         console.log(error);
@@ -56,3 +61,17 @@ function createHTML(details) {                                                  
     </section>`;
     header.innerHTML = `${details.categories[0].name.toUpperCase()}`;                       //also changing category header accordingly
 }
+
+function navOrganize(details) {
+    if (details.categories[0].name === "xbox") {
+        navXbox.classList.add("on-page");
+    }
+    else if (details.categories[0].name === "pc") {
+        navPc.classList.add("on-page");
+    }
+    else if (details.categories[0].name === "nintendo") {
+        navNintendo.classList.add("on-page");
+    } else {
+        navPlaystation.classList.add("on-page");
+    }
+};
